79ba0db33beb7ee96c53c0eb1efed54287be1c86
macarlson28 <41409283+macarlson28@users.noreply.github.com>
2018-12-05 19:24:15 -0600

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -8,35 +8,36 @@ var tbody = d3.select("tbody");
 
 //Console.log the data from data.js
 function renderTable(){
-// // Loop Through `data` and console.log each report
-data.forEach(function(UFOReport) {
-  console.log(UFOReport);
-  var row = tbody.append("tr");
-  Object.entries(UFOReport).forEach(function([key, value]) {
-    console.log(key, value);
-    var cell = row.append("td");
-    cell.text(value);
-})
-})
-}
 
-function buttonclick(){
+// // Loop Through `data` and console.log each report
+    tableData.forEach(function(UFOReport) {
+        console.log(UFOReport);
+       var row = tbody.append("tr");
+        Object.entries(UFOReport).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+    })
+})
+};
+renderTable();
+
 var submit = d3.select("#filter-btn");
 
 submit.on("click", function() {
-    var tbody = d3.select("tbody");
+    
 
   // Prevent the page from refreshing
     d3.event.preventDefault();
 
   // Select the input element and get the raw HTML node
      var inputElement = d3.select("#datetime").property("value");
-
+     console.log(inputElement)
   // Get the value property of the input element
     var filteredData = tableData.filter(d => d.datetime === inputElement);
      console.log(filteredData);
 
-    var tbody = d3.select("tbody");
+    tbody.html("");
 
     filteredData.forEach(function(entry){
         var row = tbody.append("tr")
@@ -44,9 +45,6 @@ submit.on("click", function() {
             row.append("td").text(value);
 })
  })
-})
+
 //renderTable();
-};
-
-renderTable();
-
+});
